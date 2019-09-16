20cf763e0269d9e965aa9f0de7d5a77d64206284
Paulina Aburto Navarro <paulina.aburto@usach.cl>
2019-09-16 14:50:39 -0300

    Creacion componentes Tarea

---
+++ src/components/VisualizarTarea.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <b-card>
+            <h3>Nombre de la tarea</h3>
+            <p>Fecha de la tarea</p>
+            <p>Descripcion de la tarea</p>
+            <p>Requerimientos de la tarea</p>
+            <b-row>
+                <b-col>    
+                    <b-button variant="danger">Rechazar</b-button>
+                </b-col>
+                <b-col>
+                    <b-button variant="success">Aceptar</b-button>
+                </b-col>
+            </b-row>
+        </b-card>
+    </div>
+</template>
+
